78214f56bc83fcec4daded2e61b52d67249e1b3f
loris.jacques@yahoo.fr <loris.jacques@yahoo.fr>
2025-07-15 18:40:21 +0200

    add main

---
+++ main.js
@@ -0,0 +1,20 @@
+/**
+bot-dfk/main.js
+
+Author: Loris JACQUES
+Created: Tue Jul 15 2025
+Last Modified: Tue Jul 15 2025
+Version: 1.0.0
+Description:
+*/
+
+/**
+---------------------
+external libraries
+---------------------
+*/
+/**
+---------------------
+internal libraries
+---------------------
+*/
